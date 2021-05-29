32ab12af75c949f637eda472c407846194f6e2f2
chiluen <leo545035@gmail.com>
2021-05-29 19:39:20 +0800

    modify

--- app.py
+++ app.py
@@ -30,17 +30,38 @@ def callback():
         abort(400)
     return 'OK'
 
+
 # 處理訊息
 @handler.add(MessageEvent, message=TextMessage)
 def handle_message(event):
     #text = event.message.text
-    user_id = event.source.user_id
-    message = TextSendMessage(text="請問你的星座")
-    line_bot_api.push_message(user_id, message)
-    #line_bot_api.reply_message(event.reply_token, message)
 
-    message = TextSendMessage(text=event.message.text)
-    line_bot_api.reply_message(event.reply_token, message)
+    """
+    設計邏輯：
+    沒辦法再這個function直接設計一套流程，且也沒辦法儲存variable(除非用database)
+
+    先問星座, 並且問平常傾向哪一種投資
+    然後下一次reply, 再一次回應
+    """
+    text=event.message.text
+    user_id = event.source.user_id
+    if text == "開始":
+        temp_text = "請問你的星座，以及你是風險低中高者（以數3,2,1）代表"
+        message = TextSendMessage(text=temp_text)
+        line_bot_api.push_message(user_id, message)  
+
+    elif text == "天蠍座 1":
+        """
+        把爬蟲弄進來，根據運勢去做篩選
+        """
+        message = TextSendMessage(text="投資投資")
+        line_bot_api.push_message(user_id, message)
+    else:
+        pass
+    
+    #line_bot_api.reply_message(event.reply_token, message)
+    #message = TextSendMessage(text=event.message.text)
+    #line_bot_api.reply_message(event.reply_token, message)
 
 import os
 if __name__ == "__main__":
